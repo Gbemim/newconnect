46d4cf01f603e430959e962ba4abf85be6c51d4f
gbemim <gbemisolaakerele@gmail.com>
2021-05-13 13:07:39 -0400

    connecting and adding static data to the mongodb

--- backend/data/dataprofile.js
+++ backend/data/dataprofile.js
@@ -1,23 +1,25 @@
+import bcrypt from 'bcryptjs'
 const dataprofiles = [
   {
-    _id: '1',
     name: 'Gbemi',
     image: 'images/img1.jpg',
-    about: 'This is a test run'
+    about: 'This is a test run',
+    password: bcrypt.hashSync('abc1234', 10),
+    isAdmin: true
   },
 
   {
-    _id: '2',
     name: 'Mary',
     image: 'images/img2.jpg',
-    about: 'This is a test run2'
+    about: 'This is a test run2',
+    password: bcrypt.hashSync('abc1234', 10)
   },
 
   {
-    _id: '3',
     name: 'Lisa',
     image: 'images/img3.jpg',
-    about: 'This is a test run3'
+    about: 'This is a test run3',
+    password: bcrypt.hashSync('abc1234', 10)
   },
 ]
 
---
+++ backend/models/userModel.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+  
+    about: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+
+}, {
+    timestamps: true
+})
+
+const User = mongoose.model('user', userSchema)
+
+export default User
