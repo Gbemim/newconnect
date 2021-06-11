337600a48acfbcc31846538eaf9ec184e9c17e05
gbemim <gbemisolaakerele@gmail.com>
2021-06-10 23:07:38 -0400

    intial step to implement twilio

--- backend/server.js
+++ backend/server.js
@@ -18,16 +18,16 @@ const app = express()
 
 app.use(express.json())
 
-const __dirname = path.resolve()
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
+// const __dirname = path.resolve()
+// if(process.env.NODE_ENV === 'production'){
+//     app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
-} else {
-    app.get('/', (req, res) => {
-        res.send('The API is running...')
-    })
-}
+//     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+// } else {
+//     app.get('/', (req, res) => {
+//         res.send('The API is running...')
+//     })
+// }
 
 // app.use('/api/liveusers', userRoutes)
 app.use('/api/users', oneUserRoutes)
